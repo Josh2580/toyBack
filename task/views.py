42a702df9d4d6e6d710df645e30e05c6425fa7bc
Joshua <joshuaugwuede@gmail.com>
2024-06-07 16:35:02 +0100

    adjusting DRF ui

--- task/views.py
+++ task/views.py
@@ -35,21 +35,3 @@ class TaskViewSet(viewsets.ModelViewSet):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-
-# class TaskViewSet(viewsets.ModelViewSet):
-#     queryset = Task.objects.all()
-#     serializer_class = TaskSerializer
-#     # lookup_field = 'user__telegram_id'
-#     filter_backends = [DjangoFilterBackend]
-#     filterset_fields = ['user__telegram_id']
-
-#     def partial_update(self, request, *args, **kwargs):
-#         task = self.get_object()
-#         telegram_id = request.data.get('telegram_id')
-#         user = TelegramUser.objects.get(telegram_id=telegram_id)  # Ensure this user exists and is fetched correctly
-#         if user:  
-#             task.user.add(user)  # Adding a user to the Many-to-Many relationship
-#         task.save()
-#         return Response(self.get_serializer(task).data, status=status.HTTP_200_OK)
-
-
